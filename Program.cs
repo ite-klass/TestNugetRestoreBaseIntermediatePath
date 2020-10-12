20eee93a0490b235b1d76fcc816b0aa307c6259c
Jan Klass <8045680+ite-klass@users.noreply.github.com>
2020-10-12 17:44:00 +0200

    Create test project
        msbuild -t:restore TestNugetRestoreBaseIntermediatePath.sln
    
    will not respect the MSBuild property IntermediateOutputPath. Instead of using Temp2 it produces temporary files in the obj folder (the default IntermediateOutputPath).
    
    VS in turn will not identify these as intermediate output files and show them instead of hiding them.
    
    The workspace becomes dirty.

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace TestNugetRestoreBaseIntermediatePath
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
